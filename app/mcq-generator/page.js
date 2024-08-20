a681405a6d0847bdfb52ae9835158cbd7d271903
Shivansh Anand <shivanshanand2@gmail.com>
2024-08-20 23:02:20 +0800

    build

--- app/mcq-generator/page.js
+++ app/mcq-generator/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState } from "react"; 
 
 function MCQ({ question, options, onAnswerSelect, correctAnswer, feedback }) {
   const [selectedOption, setSelectedOption] = useState(null);
